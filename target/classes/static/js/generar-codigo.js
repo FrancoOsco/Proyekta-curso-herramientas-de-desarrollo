04646e2fd7eb01930cff59879ec42a6556af1ad0
FrancoOsco <francosco0112@gmail.com>
2026-05-12 14:59:59 -0500

    Integracion de Docker PostgreSQL al proyecto Spring Boot

--- target/classes/static/js/generar-codigo.js
+++
@@ -1,9 +0,0 @@
-const generatedIdInput = document.getElementById('projectGeneratedId');
-const projectNameInput = document.getElementById('projectName');
-
-if (projectNameInput && generatedIdInput) {
-  projectNameInput.addEventListener('input', () => {
-    const generatedId = 'PRJ-' + Date.now() + '-' + Math.floor(Math.random() * 1000);
-    generatedIdInput.value = generatedId;
-  });
-}
