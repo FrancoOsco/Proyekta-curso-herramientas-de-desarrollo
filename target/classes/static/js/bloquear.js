9fefa30dc55e6fb82ab0112ee141e6294325b18a
FrancoOsco <francosco0112@gmail.com>
2026-05-12 12:49:46 -0500

    Elimina target del control de versiones

--- target/classes/static/js/bloquear.js
+++
@@ -1,6 +0,0 @@
-const termsCheckbox = document.getElementById('terms');
-                const submitButton = document.getElementById('btnRegistrar');
-
-                termsCheckbox.addEventListener('change', () => {
-                  submitButton.disabled = !termsCheckbox.checked;
-                })
